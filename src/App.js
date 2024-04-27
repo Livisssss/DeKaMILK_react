106a11c4aebdba0877555c74c79d4a9237a54ab3
Livisssss <lverlindo@yahoo.com.br>
2024-04-26 22:10:28 -0300

    Ajustes na SideBar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from "react";
+import "react-pro-sidebar/dist/css/styles.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import Cadastros from "./components/Cadastros";
-import Clientes from "./components/CadastroCliente";
 import Financeiro from "./components/Financeiro";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" exact element={<Login />} />
+        <Route element={<Header />} />
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/cadastros" element={<Cadastros />} />
         <Route path="/financeiro" element={<Financeiro />} />
-        <Route path="/cadastroCliente" element={<Clientes />} />
       </Routes>
     </Router>
   );
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,26 @@
-// Header.js
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import "./Header.css"; // Importe o arquivo de estilo aqui
+import "./Header.css";
 import BarraLateral from "../BarraLateral";
 
 const Header = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const [barraLateralAberta, setBarraLateralAberta] = useState(false);
+  const [sidebarAberta, setSidebarAberta] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarAberta(!sidebarAberta);
+  };
 
   const logout = () => {
     navigate("/");
-  };
-
-  const toggleBarraLateral = () => {
-    setBarraLateralAberta(!barraLateralAberta);
   };
 
   if (location.pathname !== "/") {
     return (
       <>
         <header className="header">
-          <button onClick={toggleBarraLateral} className="btMenu">
+          <button className="btMenu" onClick={toggleSidebar}>
             <img src="/imgs/menu_icon.png" alt="Menu" className="icon" />
           </button>
           <h1>
@@ -31,8 +30,10 @@ const Header = () => {
             SAIR
           </button>
         </header>
-
-        <BarraLateral aberta={barraLateralAberta} />
+        {sidebarAberta && (
+          <div className="overlay" onClick={toggleSidebar}></div>
+        )}
+        <BarraLateral aberta={sidebarAberta} />
       </>
     );
   } else {
