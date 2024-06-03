e89e5241eb9fa02a9073c92ff97ec2816747ab92
brunosimionato <101061@aluno.uricer.edu.br>
2024-06-03 20:26:13 -0300

    alinhando detalhes e iniciando o btEditar

--- src/Cadastros/Cliente/CadastroCliente/index.js
+++ src/Cadastros/Cliente/CadastroCliente/index.js
@@ -1,120 +1,271 @@
 import React, { useState, useEffect } from "react";
 import Header from "../../../components/Header";
+import { useNavigate, useParams } from "react-router-dom";
 import "./cadastroCliente.css";
 import MenuCadastros from "../../components/MenuCadastros";
 
 const CadastroCliente = () => {
+  const { clienteId } = useParams();
+
+  const navigate = useNavigate();
+  
   const [telaCadastrosAberta, setTelaCadastrosAberta] = useState("CLIENTES");
   const [nome, setNome] = useState("");
   const [cpf, setCpf] = useState("");
-  const [rg, setRg] = useState("");
+  const [cnpj, setCnpj] = useState("");
   const [uf, setUf] = useState("");
   const [cep, setCep] = useState("");
   const [telefone, setTelefone] = useState("");
   const [endereco, setEndereco] = useState("");
   const [cidade, setCidade] = useState("");
   const [email, setEmail] = useState("");
-  const [showPopup, setShowPopup] = useState(false);
-  const [fadeOut, setFadeOut] = useState(false);
+  const [tipoPessoa, setTipoPessoa] = useState(null);
+  const [idCliente, setIdCliente] = useState(null); // Corrigido para setClienteId
+  
+  useEffect(() => {
+    if (clienteId) {
+      setIdCliente(clienteId);
+      fetchCliente(clienteId);
+    }
+  }, [clienteId]);
+  
+
+  const fetchCliente = async (clienteId) => {
+    try {
+      const response = await fetch(`http://localhost:3000/api/v1/cliente/${clienteId}`);
+      if (!response.ok) {
+        throw new Error("Erro ao buscar os detalhes do cliente.");
+      }
+      const cliente = await response.json();
+      // Atualizar o estado do componente com os detalhes do cliente obtidos da API
+      setNome(cliente.nome);
+      setCpf(cliente.cpf);
+      setCnpj(cliente.cnpj);
+      setEndereco(cliente.endereco);
+      setCidade(cliente.cidade);
+      setUf(cliente.uf);
+      setCep(cliente.cep);
+      setTelefone(cliente.telefone);
+      setEmail(cliente.email);
+      // Atualize os outros campos conforme necessário
+    } catch (error) {
+      console.error("Erro ao buscar os detalhes do cliente:", error);
+      // Tratar o erro aqui, por exemplo, exibindo uma mensagem para o usuário
+    }
+  };
+  
+  
+
+
 
   const handleButtonClick = (tela) => {
     setTelaCadastrosAberta(tela);
   };
 
-  // APLICA A MASCARA CPF E ATUALIZA O INPUT
   const handleChangeCpf = (event) => {
     let value = event.target.value;
     value = value.replace(/\D/g, "");
     value = cpfMascara(value);
-
     setCpf(value);
   };
 
-  // APLICA A MASCARA RG E ATUALIZA O INPUT
-  const handleChangeRg = (event) => {
+  const handleChangeCnpj = (event) => {
     let value = event.target.value;
-    value = rgMascara(value);
-
-    setRg(value);
+    value = cnpjMascara(value);
+    setCnpj(value);
   };
 
-  // APLICA A MASCARA UF E ATUALIZA O INPUT
   const handleChangeUf = (event) => {
     let value = event.target.value;
-    // Aplica a máscara
     value = ufMascara(value);
-
     setUf(value);
   };
 
-  // APLICA A MASCARA CEP E ATUALIZA O INPUT
   const handleChangeCep = (event) => {
     let value = event.target.value;
     value = cepMascara(value);
-
     setCep(value);
   };
 
-  // APLICA A MASCARA DE TELEFONE E ATUALIZA O INPUT
   const handleChangeTelefone = (event) => {
     let value = event.target.value;
     value = telefoneMascara(value);
-
     setTelefone(value);
   };
 
-  // FUNÇÃO PARA VERIFICAR CAMPOS OBRIGATÓRIOS
   const verificarCamposObrigatorios = () => {
-    return nome && cpf && rg && telefone && email;
+    if (tipoPessoa === "fisica") {
+      return nome && telefone && email && cpf && cpf.length === 14;
+    } else if (tipoPessoa === "juridica") {
+      return nome && telefone && email && cnpj && cnpj.length === 18;
+    } else {
+      return false; // Se o tipo de pessoa for nulo, retorna falso
+    }
   };
 
-  // FUNÇÃO PARA FECHAR POPUP
-  const fecharPopup = () => {
-    setFadeOut(true);
-    setTimeout(() => {
-      setShowPopup(false);
-      setFadeOut(false);
-    }, 300);
+  const handleTipoPessoaChange = (newTipoPessoa) => {
+    setTipoPessoa(newTipoPessoa);
+    if (newTipoPessoa === "fisica") {
+      setCnpj(""); // Limpa o CNPJ ao selecionar Pessoa Física
+    } else {
+      setCpf(""); // Limpa o CPF ao selecionar Pessoa Jurídica
+    }
   };
 
-  // FECHA O POP UP COM A TECLA ESC
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      if (event.keyCode === 27 && showPopup) {
-        fecharPopup();
+  const handleAlterarClick = async () => {
+    if (!idCliente) {
+      console.error("ID do cliente não definido.");
+      return;
+    }
+
+    console.log("Botão de alterar clicado");
+
+    const cliente = {
+      nome,
+      cpf: tipoPessoa === "fisica" ? cpf : undefined,
+      cnpj: tipoPessoa === "juridica" ? cnpj : undefined,
+      endereco,
+      cidade,
+      uf,
+      cep,
+      telefone,
+      email,
+    };
+
+    try {
+      const response = await fetch(`http://localhost:3000/api/v1/cliente/${idCliente}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cliente),
+      });
+
+      if (response.ok) {
+        alert("Cliente alterado com sucesso.");
+        limparCampos(); // Chama a função para limpar os campos
+      } else {
+        const result = await response.json();
+        console.error("Erro ao alterar cliente:", result);
+        alert("Erro ao alterar cliente. Verifique os dados e tente novamente.");
       }
-    };
+    } catch (error) {
+      console.error("Erro ao alterar cliente:", error);
+      alert("Erro ao alterar cliente. Tente novamente.");
+    }
+  };
 
-    document.addEventListener("keydown", handleKeyPress);
-
-    return () => {
-      document.removeEventListener("keydown", handleKeyPress);
-    };
-  }, [showPopup]); // Certifique-se de adicionar showPopup como dependência para atualizar o evento conforme necessário
-
-  // FUNÇÃO BOTÃO LIMPAR
   const limparCampos = () => {
     setNome("");
     setCpf("");
-    setRg("");
+    setCnpj("");
     setEndereco("");
     setCidade("");
     setUf("");
     setCep("");
     setTelefone("");
     setEmail("");
-    setCidade("");
+    setTipoPessoa("null");
   };
 
-  // FUNÇÃO BOTÃO INCLUIR
-  const incluirCliente = () => {
+
+
+  // BOTÃO INCLUIR
+  const incluirCliente = async () => {
+    // Verifica se todos os campos obrigatórios estão preenchidos
     if (!verificarCamposObrigatorios()) {
-      setShowPopup(true);
+      alert("Por favor, preencha todos os campos obrigatórios.");
       return;
     }
-    // Adicione aqui a lógica para enviar os dados do cliente
+  
+    // Verifica se os campos com máscara estão preenchidos corretamente
+    if (tipoPessoa === "fisica") {
+      if (!cpf || cpf.length !== 14) {
+        alert("Por favor, preencha o CPF corretamente.");
+        return;
+      }
+    } else if (tipoPessoa === "juridica") {
+      if (!cnpj || cnpj.length !== 18) {
+        alert("Por favor, preencha o CNPJ corretamente.");
+        return;
+      }
+    }
+  
+    if (!cep || cep.length !== 9) {
+      alert("Por favor, preencha o CEP corretamente.");
+      return;
+    }
+  
+    if (!telefone || telefone.length !== 16) {
+      alert("Por favor, preencha o telefone corretamente.");
+      return;
+    }
+  
+    // Agora envia os dados para o servidor
+    const cliente = {
+      nome,
+      cpf: tipoPessoa === "fisica" ? cpf : undefined, // Só envia CPF se for pessoa física
+      cnpj: tipoPessoa === "juridica" ? cnpj : undefined, // Só envia CNPJ se for pessoa jurídica
+      endereco,
+      cidade,
+      uf,
+      cep,
+      telefone,
+      email,
+    };
+  
+    console.log("Enviando cliente:", cliente);
+  
+    try {
+      const response = await fetch("http://localhost:3000/api/v1/cliente", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cliente),
+      });
+  
+      const result = await response.json();
+  
+      if (response.ok) {
+        limparCampos();
+      } else if (response.status === 422) {
+        console.error("Erro de validação:", result);
+        if (result.error === 'Já existe um cliente cadastrado com este CPF.') {
+          alert("Já existe um cliente cadastrado com este CPF.");
+          setCpf(""); // Limpa o campo CPF
+        } else if (result.error === 'Já existe um cliente cadastrado com este CNPJ.') {
+          alert("Já existe um cliente cadastrado com este CNPJ.");
+          setCnpj(""); // Limpa o campo CNPJ
+        } else {
+          alert("Erro ao incluir cliente. Verifique os dados e tente novamente.");
+        }
+      } else {
+        console.error("Erro inesperado:", result);
+        alert("Erro ao incluir cliente. Tente novamente.");
+      }
+    } catch (error) {
+      console.error("Erro ao incluir cliente:", error);
+      alert("Erro ao incluir cliente. Tente novamente.");
+    }
   };
+  
 
+
+
+
+
+
+
+  
+
+
+
+
+  
+  
+  
+  
   return (
     <div>
       <Header />
@@ -128,14 +279,25 @@ const CadastroCliente = () => {
         <label htmlFor="nome">NOME*</label>
         <input type="text" id="nome" value={nome} onChange={(e) => setNome(e.target.value)} maxLength="60" />
 
-        <div className="cpf-rg-container">
+        <div className="radio-buttons">
+          <div className="radio-button-container">
+            <input type="radio" value="fisica" name="tipoPessoa" checked={tipoPessoa === "fisica"} onChange={() => handleTipoPessoaChange("fisica")} />
+            <label>Pessoa Física</label>
+          </div>
+          <div className="radio-button-container">
+            <input type="radio" value="juridica" name="tipoPessoa" checked={tipoPessoa === "juridica"} onChange={() => handleTipoPessoaChange("juridica")} />
+            <label>Pessoa Jurídica</label>
+          </div>
+        </div>
+
+        <div className="cpf-cnpj-container">
           <div>
             <label htmlFor="cpf">CPF*</label>
-            <input type="text" id="cpf" name="cpf" value={cpf} onChange={handleChangeCpf} maxLength="14" />
+            <input type="text" id="cpf" name="cpf" value={cpf} onChange={handleChangeCpf} maxLength="14" disabled={tipoPessoa !== "fisica"} />
           </div>
           <div>
-            <label htmlFor="rg">RG*</label>
-            <input type="text" id="rg" name="rg" value={rg} onChange={handleChangeRg} maxLength="10" />
+            <label htmlFor="cnpj">CNPJ*</label>
+            <input type="text" id="cnpj" name="cnpj" value={cnpj} onChange={handleChangeCnpj} maxLength="18" disabled={tipoPessoa !== "juridica"} />
           </div>
         </div>
 
@@ -173,23 +335,13 @@ const CadastroCliente = () => {
         <div className="botoes-esquerda">
           <button type="button" name="btIncluir" id="btIncluir" onClick={incluirCliente}>INCLUIR</button>
           <button type="button" name="btDeletar" id="btDeletar">DELETAR</button>
-          <button type="button" name="btAlterar" id="btAlterar">ALTERAR</button>
+          <button type="button" name="btAlterar" id="btAlterar" onClick={handleAlterarClick}>ALTERAR</button>
         </div>
         <div className="botoes-direita">
           <button type="button" name="btLimpar" id="btLimpar" onClick={limparCampos}>LIMPAR</button>
           <button type="button" name="btConsultar" id="btConsultar">CONSULTAR</button>
         </div>
       </div>
-
-      {showPopup && (
-      <div className={`popup-overlay ${fadeOut ? 'fade-out' : ''}`}>
-        <div className={`popup ${fadeOut ? 'fade-out' : ''}`}>
-          <p>Por favor, preencha todos os campos <br /> obrigatórios.</p>
-          <button onClick={fecharPopup}>Fechar</button>
-        </div>
-      </div>
-    )}
-
     </div>
   );
 };
@@ -204,11 +356,16 @@ export const cpfMascara = (value) => {
     .replace(/(-\d{2})\d+?$/, "$1");
 };
 
-// MÁSCARA PARA RG
-export const rgMascara = (value) => {
+// MÁSCARA PARA CNPJ
+export const cnpjMascara = (value) => {
   const formattedValue = value.replace(/\D/g, "");
 
-  return formattedValue.slice(0, 10);
+  return formattedValue
+    .slice(0, 14)
+    .replace(/^(\d{2})(\d)/, "$1.$2")
+    .replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+    .replace(/\.(\d{3})(\d)/, ".$1/$2")
+    .replace(/(\d{4})(\d)/, "$1-$2");
 };
 
 // MÁSCARA UF
--- src/Cadastros/Cliente/ClienteInicial/index.js
+++ src/Cadastros/Cliente/ClienteInicial/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Header from "../../../components/Header";
 import "./ClienteInicial.css";
 import MenuCadastros from "../../components/MenuCadastros";
@@ -9,6 +9,8 @@ import { faPenToSquare } from '@fortawesome/free-regular-svg-icons';
 const ClienteInicial = () => {
   const [telaCadastrosAberta, setTelaCadastrosAberta] = useState("CLIENTES");
   const [clientes, setClientes] = useState([]);
+  const [clienteEditData, setClienteEditData] = useState(null); // Estado para os dados do cliente a serem editados
+  const navigate = useNavigate();
 
   const handleButtonClick = (tela) => {
     setTelaCadastrosAberta(tela);
@@ -25,11 +27,38 @@ const ClienteInicial = () => {
         setClientes(clientesData);
       } catch (error) {
         console.error("Erro ao buscar os clientes:", error);
+        // Trate o erro aqui, por exemplo, exibindo uma mensagem para o usuário
       }
     };
 
     fetchClientes();
   }, []);
+
+  const handleEditClick = async (clienteId, cliente) => {
+    try {
+      if (!clienteId) {
+        throw new Error("ID do cliente não fornecido.");
+      }
+  
+      // Aqui você pode fazer a solicitação GET para buscar os detalhes do cliente pelo ID
+      const response = await fetch(`http://localhost:3000/api/v1/cliente/${clienteId}`);
+      if (!response.ok) {
+        throw new Error("Erro ao buscar detalhes do cliente.");
+      }
+      const clienteData = await response.json();
+
+      console.log("Detalhes do cliente retornado:", clienteData.nome);
+      
+      // Salvar os detalhes do cliente no estado para exibir nos campos do formulário de edição
+      setClienteEditData(clienteData);
+
+      navigate("/cadastroCliente");
+    } catch (error) {
+      console.error("Erro ao buscar os detalhes do cliente:", error);
+      // Trate o erro aqui, por exemplo, exibindo uma mensagem para o usuário
+    }
+  };
+  
 
   
   return (
@@ -43,8 +72,10 @@ const ClienteInicial = () => {
         <table>
           <thead>
             <tr>
+              <th>ID</th>
               <th>NOME</th>
               <th>CPF</th>
+              <th>CNPJ</th>
               <th>ENDEREÇO</th>
               <th>CIDADE</th>
               <th>UF</th>
@@ -55,10 +86,12 @@ const ClienteInicial = () => {
             </tr>
           </thead>
           <tbody>
-            {clientes.map((cliente) => (
-              <tr key={cliente.id}>
+            {clientes.map((cliente, index) => (
+              <tr key={index}>
+                <td>{cliente.cliente_id}</td>
                 <td>{cliente.nome}</td>
                 <td>{cliente.cpf}</td>
+                <td>{cliente.cnpj}</td>
                 <td>{cliente.endereco}</td>
                 <td>{cliente.cidade}</td>
                 <td>{cliente.uf}</td>
@@ -66,7 +99,7 @@ const ClienteInicial = () => {
                 <td>{cliente.telefone}</td>
                 <td>{cliente.email}</td>
                 <td>
-                  <button className="editar-button" data-cliente-id={cliente.id}>
+                  <button className="editar-button" onClick={() => handleEditClick(cliente.cliente_id, cliente)}>
                     <FontAwesomeIcon icon={faPenToSquare} />
                   </button>
                 </td>
